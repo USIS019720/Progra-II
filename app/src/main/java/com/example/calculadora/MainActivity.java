dff7b18d2fa4564c7f33674ebf8f1b457587b4d4
David <davidernestocernaavalos848@gmail.com>
2021-02-06 15:32:19 -0600

    se creo y se subio la rama Conversor_Unidades

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -14,54 +14,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void calcular(View view){
-        TextView tempVal = (TextView)findViewById(R.id.txtnum1);
-        double num1 = Double.parseDouble(tempVal.getText().toString());
 
-        tempVal = (TextView)findViewById(R.id.txtnum2);
-        double num2 = Double.parseDouble(tempVal.getText().toString());
-
-        double respuesta = 1;
-
-        Spinner cboOperacionesBasicas = findViewById(R.id.cboOperacionesBasicas);
-        switch (cboOperacionesBasicas.getSelectedItemPosition()) {
-            case 0://suma
-                respuesta = num1 + num2;
-                break;
-
-            case 1://Resta
-                respuesta = num1 - num2;
-                break;
-
-            case 2://Multiplicacion
-                respuesta = num1 * num2;
-                break;
-
-            case 3://Division
-                respuesta = num1 / num2;
-                break;
-
-            case 5: //Exponente
-                respuesta = Math.pow(num1, num2);
-                break;
-
-            case 6: //Raiz
-                respuesta = Math.pow(num1, 1/num2);
-                break;
-
-            case 7: //Mod
-                respuesta = num1 % num2;
-                break;
-
-            case 8: //Num mayor
-                if (num1 > num2) {
-                    respuesta = num1;
-                } else  {
-                    respuesta = num2;
-                }
-                break;
-        }
-        tempVal = findViewById(R.id.lblRespuesta);
-        tempVal.setText("Respuesta: "+ respuesta );
-    }
 }

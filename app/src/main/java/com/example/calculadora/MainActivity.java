7188b6968a2797204fdbb72d8223a6f7fecfaaf3
David <davidernestocernaavalos848@gmail.com>
2021-03-07 11:34:23 -0600

    se subio el icono del contador de agua y la pestaña de este

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -15,6 +15,24 @@ import android.widget.Toast;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+    TabHost tbhagua;
+    TextView temval;
+    Button btnconvertir;
 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tbhagua = findViewById(R.id.tbhagua);
+        tbhagua.setup();
+
+        temval = findViewById(R.id.lblRespuesta);
+
+        tbhagua.addTab(tbhagua.newTabSpec( "agua").setContent(R.id.tabagua).setIndicator("",getResources().getDrawable(R.drawable.ic_agua)));
+
+
+    }
 
     }

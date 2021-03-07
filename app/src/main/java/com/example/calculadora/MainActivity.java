aeeb2a22798c023db6ecf3d79c2d28b780496c07
Fernando Antonio Quintanilla Soriano <usis007920@ugb.edu.sv>
2021-03-07 11:53:14 -0600

    Se agregaron las operaciones para el convertidor de agua y se agrego la pestaña de area

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -7,6 +7,7 @@ import android.nfc.TagLostException;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TabHost;
 import android.widget.TextView;
@@ -31,8 +32,33 @@ public class MainActivity extends AppCompatActivity {
         temval = findViewById(R.id.lblRespuesta);
 
         tbhagua.addTab(tbhagua.newTabSpec( "agua").setContent(R.id.tabagua).setIndicator("",getResources().getDrawable(R.drawable.ic_agua)));
+        tbhagua.addTab(tbhagua.newTabSpec( "agua").setContent(R.id.tabagua).setIndicator("",getResources().getDrawable(R.drawable.ic_area)));
 
 
+    }
+
+    public void calcular(View v){
+        TextView tempVal = (TextView)findViewById(R.id.txtnum1);
+        double num1 = Double.parseDouble(tempVal.getText().toString());
+
+        double respuesta = 1;
+        RadioButton optOperacionesBasicas = findViewById(R.id.optcaso1);
+        if( optOperacionesBasicas.isChecked() ) {
+            respuesta = num1 = 6 ;
+        }
+
+        optOperacionesBasicas = findViewById(R.id.optcaso2);
+        if( optOperacionesBasicas.isChecked() ) {
+            respuesta = (num1-18) * 0.45 + 6;
+        }
+
+        optOperacionesBasicas = findViewById(R.id.optcaso3);
+        if( optOperacionesBasicas.isChecked() ) {
+            respuesta = (num1-28)*0.65+(28-18)*0.45+6;
+        }
+
+        tempVal = findViewById(R.id.lblRespuesta);
+        tempVal.setText("Respuesta: "+ respuesta );
     }
 
     }
